2fd886d197a452db9e8124c89d23397dfcf23a69
pinones97 <pinones.810@gmail.com>
2023-03-17 16:28:19 -0700

    clean up code

--- app.js
+++ app.js
@@ -9,14 +9,12 @@ let move = 0;
 const game = document.getElementById("game_box");
 let apple = Math.floor(Math.random() * 900);
 
-
-
 function gameLayout(){
-    game.innerHTML = "";
+    game.innerHTML = '';
 
     for (let i = 0; i < 900; i++) {
-        const gridCell = document.createElement("div");
-        gridCell.classList.add("grid_cell");
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('grid_cell');
         game.appendChild(gridCell);
     };
 
@@ -26,16 +24,16 @@ function gameLayout(){
         direction: [0],
     };
 
-    const gridList = document.querySelectorAll("#game_box .grid_cell");
-    gridList[apple].classList.add("apple");
+    const gridList = document.querySelectorAll('#game_box .grid_cell');
+    gridList[apple].classList.add('apple');
     
     setInterval(update, 200);
 };
 
 function renderState() {
-    const gridList = document.querySelectorAll("#game_box .grid_cell");
+    const gridList = document.querySelectorAll('#game_box .grid_cell');
     snake.body.forEach(function(index) {
-        gridList[index].classList.add("snake");
+        gridList[index].classList.add('snake');
     });
 };
 
@@ -43,8 +41,10 @@ function moveSnake() {
     snake.direction = snake.body[snake.body.length-1];
     snake.direction += move;
 
-    const gridList = document.querySelectorAll("#game_box .grid_cell");
-    gridList[snake.body[0]].classList.remove("snake");
+    //if move is overbond end game
+
+    const gridList = document.querySelectorAll('#game_box .grid_cell');
+    gridList[snake.body[0]].classList.remove('snake');
 
     snake.body.shift();
     snake.body.push(snake.direction);
@@ -52,44 +52,64 @@ function moveSnake() {
     renderState();
 };
 
+
 function growSnake() {
     if (snake.body[snake.body.length-1] == apple) {
         snake.direction = apple;
         snake.direction += move;
     
-        const gridList = document.querySelectorAll("#game_box .grid_cell");
-        gridList[snake.body[snake.body.length-1]].classList.add("snake");
-        gridList[apple].classList.remove("apple");
+        const gridList = document.querySelectorAll('#game_box .grid_cell');
+        gridList[snake.body[snake.body.length-1]].classList.add('snake');
+        gridList[apple].classList.remove('apple');
 
         snake.body.push(snake.direction);
          
         apple = Math.floor(Math.random() * 900);
-        gridList[apple].classList.add("apple");
+        //apple = getApplePo();
+        gridList[apple].classList.add('apple');
     };
 };
+
+function score(){
+    if (snake.body.length > 2){
+        document.getElementById("score").innerText = "SOCRE: " + (snake.body.length-2);
+    };
+};
+
+// function getApplePo(){
+//     let position = Math.floor(Math.random() * 900);
+//     //check if this in inside snake
+
+//     //if not return position
+        
+//     //if so return getApplePo()
+
+// };
+
 
 function update() {
     renderState();
     moveSnake();
     growSnake();
+    score();
 };
 
 
-document.getElementById("start_game").addEventListener('click', function() {
+document.getElementById('start_game').addEventListener('click', function() {
     gameLayout();
 });
 
 document.addEventListener('keydown', function (event) {
     if (event.code == 'ArrowRight') {
         move = 1;
-   }
-   else if (event.code == 'ArrowLeft') {
+    }
+    else if (event.code == 'ArrowLeft') {
         move = -1;          
-   }
+    }
     else if (event.code == 'ArrowUp') {
         move = -30;
-   }
+    }
     else if (event.code == 'ArrowDown') {
         move = 30;
-   };
+    };
 });
